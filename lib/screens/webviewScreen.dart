22b6c31e14afa684457e25fd489dbe2a80bae7f6
kevappdev <kevappdev@gmail.com>
2023-11-29 20:34:26 +0100

    Set app icon

--- lib/screens/webviewScreen.dart
+++ lib/screens/webviewScreen.dart
@@ -10,16 +10,24 @@ class WebviewScreen extends StatefulWidget {
 
 class _WebviewScreenState extends State<WebviewScreen> {
 
-  WebViewController controller = WebViewController()
+  WebViewController controller = WebViewController(
+
+  )
     ..setJavaScriptMode(JavaScriptMode.unrestricted)
     ..setBackgroundColor(const Color(0x00000000))
-    ..loadRequest(Uri.parse('https://web.whatsapp.com'));
+    ..loadRequest(Uri.parse('https://web.whatsapp.com'))
+    ..setUserAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1.2 Safari/605.1.15");
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: WebViewWidget(
-        controller: controller,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.only(bottom: 12.0),
+          child: WebViewWidget(
+            controller: controller,
+          ),
+        ),
       ),
     );
   }
